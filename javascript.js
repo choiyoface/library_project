2b11f1b549b06a4bb8d4f944a04de4e7c4b8ddf1
최선희 <choiyoface@gmail.com>
2023-08-01 11:47:58 +0200

    add js to form to print the input to bookDiv

--- javascript.js
+++ javascript.js
@@ -1,11 +1,59 @@
 let myLibrary = [];
 
-function Book (name, author, genre){
-    this.name = name
+function Book (title, author, genre, status){
+    this.title = title
     this.author = author
     this.genre = genre
+    this.status = status
 }
 
-const bookOne = new Book("A", "B" , "C")
+function render() {
+    let libraryEl = document.querySelector("#bookDiv");
+    libraryEl.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+      let book = myLibrary[i]; 
+  
+      let bookEl = document.createElement("div");
+      bookEl.innerHTML = `
+        <div class="bookOneTitle">${book.title}</div>
+        <div class="bookOneAuthor">${book.author}</div>
+        <div class="bookOneGenre">${book.genre}</div>
+        <div class="bookOneStatus">${book.status}</div>
+      `;
+      libraryEl.appendChild(bookEl);
+    }
+  }
+  
+  function addBookToLibrary() {
+    let title = document.querySelector("#bookTitle").value;
+    let author = document.querySelector("#bookAuthor").value;
+    let genre = document.querySelector("#bookGenre").value;
+    let status = document.querySelector("#status").value;
+    let newbook = new Book(title, author, genre, status);
+    myLibrary.push(newbook);
+    render();
+  }
+  
+  document.querySelector("#newbook-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    addBookToLibrary();
+  });
+  
 
-console.log(bookOne.name)
+
+function addBookToLibrary(){
+    let title = document.querySelector("#bookTitle").value
+    let author = document.querySelector("#bookAuthor").value
+    let genre = document.querySelector("#bookGenre").value
+    let status = document.querySelector("#status").value
+    let newbook = new Book(title, author, genre, status)
+    //create a card//
+    myLibrary.push(newbook);
+    render();
+}
+
+document.querySelector("#newbook-form").addEventListener("submit", function(event){
+    event.preventDefault();
+    addBookToLibrary();
+})
+
